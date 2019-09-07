b17fd2663d21fa4f200e7829fae462cdbf30121a
Byungman Lee <pavane90@gmail.com>
2019-09-07 11:28:54 +0900

    3. 마스터/상세정보 컴포넌트

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HeroesComponent } from "./heroes/heroes.component";
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent],
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
